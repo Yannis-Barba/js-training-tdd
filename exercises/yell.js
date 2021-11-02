c660e0e56d2a5abe30972bd15852c84c768f393c
Yannis <yannis.barba.dev@gmail.com>
2021-11-02 22:13:22 +0100

    wrote tests and resolved yell.js  from str

--- exercises/yell.js
+++ exercises/yell.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create a `yell` function that takes a string
@@ -8,8 +8,16 @@
 
 // Your code:
 
-//* Begin of tests
-const assert = require('assert');
+const yell = (str) => str.toUpperCase();
 
-assert.fail('You must write your own tests');
+//* Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof yell, "function");
+assert.strictEqual(yell.length, 1);
+assert.deepStrictEqual(yell("ma string"), "MA STRING");
+assert.deepStrictEqual(yell("Ma String"), "MA STRING");
+assert.deepStrictEqual(yell(" "), " ");
+
+// assert.fail('You must write your own tests');
 // End of tests */
